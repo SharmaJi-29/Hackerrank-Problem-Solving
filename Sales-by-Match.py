fb6b09161671f9376cd91f5f70c771cde716f805
Mrityunjay Sharma <sharmamrityunjay78@gmail.com>
2025-08-04 12:20:19 +0530

    Add files via upload

---
+++ Sales-by-Match.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    distinct_values = set(ar)
+    pairs = 0
+    for i in distinct_values:
+        ar_presence = ar.count(i)
+        pairs = math.floor(ar_presence / 2) + pairs
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
